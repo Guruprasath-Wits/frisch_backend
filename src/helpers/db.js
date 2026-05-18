d37cb2daedc8743644109b6f78a0f73865bba8f5
Natesh kumar <nateshkumar1406@gmail.com>
2026-05-18 23:50:26 +0530

    Latest Code

--- src/helpers/db.js
+++ src/helpers/db.js
@@ -6,14 +6,14 @@ console.error("!!! DB.JS LOADED - STARTING CONFIG !!!");
 
 // Database configuration
 const dbConfig = {
-  // host: "217.154.8.230",
-  host: "localhost",
-  // database: "Frisch",
-  // user: "phpmyadmin",
-  // password: "Dortmund@2106",
-  user: "root",
-  password: "",
+  host: "217.154.8.230",
+  // host: "localhost",
   database: "Frisch",
+  user: "phpmyadmin",
+  password: "Dortmund@2106",
+  // user: "root",
+  // password: "",
+  // database: "Frisch",
   port: 3306,
   waitForConnections: true,
   connectionLimit: 10, // Keep multiple connections open
